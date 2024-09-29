d367ff5059f440b661022fd9e6efb9aaf1e7c7be
shamam <kafri.shamam@gmail.com>
2024-09-29 07:26:27 +0300

    Initial Commit

---
+++ profile/profileApp.swift
@@ -0,0 +1,17 @@
+//
+//  profileApp.swift
+//  profile
+//
+//  Created by Shamam Alkafri on 29/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct profileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
